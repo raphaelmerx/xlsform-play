ee5d30642e2e6431f2ada69ab05211d1f825f085
Raphael Merx <raphael.merx@gmail.com>
2023-07-22 14:03:34 +0800

    Allow selecting example, show validation error
    + make hottable resizable, let users add questions, etc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Handsontable from 'handsontable/base';
 import { HotTable } from '@handsontable/react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { registerAllModules } from 'handsontable/registry';
@@ -15,17 +14,8 @@ import { saveAs } from 'file-saver';
 
 registerAllModules();
 
-function App() {
-  const [hotData, setHotData] = useState({});
-  const [previewUrl, setPreviewUrl] = useState('');
-
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (evt) => {
-      // Parse the file
-      const bstr = evt.target.result;
-      const wb = read(bstr, { type: 'binary' });
+const getSheetsData = (file) => {
+      const wb = read(file, { type: 'binary' });
 
       // Get all worksheets
       const sheetNames = wb.SheetNames;
@@ -35,14 +25,42 @@ function App() {
         const ws = wb.Sheets[name];
         let data = utils.sheet_to_json(ws, { header: 1 });
         data = data.filter((row) => row.length);
-        
+
         sheetsData[name] = data;
       });
+      return sheetsData;
+}
 
-      setHotData(sheetsData);
+function App() {
+  const [hotData, setHotData] = useState({});
+  const [selectedFile, setSelectedFile] = useState('');
+  const [previewUrl, setPreviewUrl] = useState('');
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      const bstr = evt.target.result;
+      setHotData(getSheetsData(bstr))
     };
     reader.readAsBinaryString(file);
   }
+
+  const handleSelectChange = async (event) => {
+    const fileName = event.target.value;
+    setSelectedFile(fileName);
+    if (!fileName) return;
+
+    const response = await fetch(`/xlsform_examples/${fileName}`);
+    const blob = await response.blob();
+    var file = new File([blob], fileName, {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});
+    const reader = new FileReader();
+    reader.onload = async (evt) => {
+      const bstr = evt.target.result;
+      setHotData(getSheetsData(bstr))
+    };
+    reader.readAsArrayBuffer(file);
+  };
 
   const constructSpreadsheet = () => {
     const wb = utils.book_new();
@@ -78,6 +96,13 @@ function App() {
         body: formData
       });
 
+      // if it's a 400 error, show the message in response.json()["error"]
+      if (response.status === 400) {
+        const data = await response.json();
+        alert(data["error"]);
+        return;
+      }
+
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
@@ -96,17 +121,28 @@ function App() {
     <Allotment defaultSizes={[100, 200]} className='scrollable-allotment'>
       <Allotment.Pane minSize={200} preferredSize="80%">
         <div className="App">
-          <input type="file" onChange={handleFileChange} accept=".xls,.xlsx" />
+          <input type="file" onChange={handleFileUpload} accept=".xls,.xlsx" />
+          OR
+          <select onChange={handleSelectChange}>
+            <option value="">Select an example spreadsheet</option>
+            <option value="anc_visit.xlsx">anc_visit.xlsx</option>
+            <option value="baseline_household_survey.xlsx">baseline_household_survey.xlsx</option>
+            <option value="fatal_injury_surveillance_form.xlsx">fatal_injury_surveillance_form.xlsx</option>
+            <option value="household_water_survey.xlsx">household_water_survey.xlsx</option>
+            <option value="monthly_project_report.xlsx">monthly_project_report.xlsx</option>
+            <option value="shelter_material_survey.xlsx">shelter_material_survey.xlsx</option>
+            <option value="spraying_survey.xlsx">spraying_survey.xlsx</option>
+          </select>
           {Object.keys(hotData).length > 0 && (<button onClick={handleFileDownload}>Download</button>)}
           {Object.keys(hotData).length > 0 && (<button onClick={handleFilePreview}>Preview</button>)}
           <Tabs>
             <TabList>
                 {Object.keys(hotData).map((sheetName) => (
-                <Tab>{sheetName}</Tab>
+                <Tab key={sheetName}>{sheetName}</Tab>
                 ))}
             </TabList>
             {Object.keys(hotData).map((sheetName) => (
-              <TabPanel>
+              <TabPanel key={sheetName}>
                 <HotTable
                   data={hotData[sheetName]}
                   rowHeaders={true}
@@ -114,6 +150,10 @@ function App() {
                   height="auto"
                   colWidths={100}
                   licenseKey="non-commercial-and-evaluation" // for non-commercial use only
+                  dropdownMenu={true}
+                  contextMenu={true}
+                  manualRowMove={true}
+                  manualColumnResize={true}
                 />
               </TabPanel>
             ))}
