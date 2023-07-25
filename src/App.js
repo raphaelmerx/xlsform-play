439da01484ac4e161f04e652d8b7579697a6035c
Raphael Merx <raphael.merx@gmail.com>
2023-07-25 17:52:21 +0800

    Remove col headers; add prettier

--- src/App.js
+++ src/App.js
@@ -12,41 +12,40 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
 
-import { Allotment } from "allotment";
-import "allotment/dist/style.css";
+import { Allotment } from 'allotment';
+import 'allotment/dist/style.css';
 
 import 'handsontable/dist/handsontable.full.min.css';
 import 'react-tabs/style/react-tabs.css';
 import { read, utils, write } from 'xlsx';
 import { saveAs } from 'file-saver';
 
-
 registerAllModules();
 
-const getSheetsData = (file) => {
-      const wb = read(file, { type: "binary", cellStyles: true });
+const getSheetsData = file => {
+  const wb = read(file, { type: 'binary', cellStyles: true });
 
-      // Get all worksheets
-      const sheetNames = wb.SheetNames;
-      let sheetsData = {};
-      let sheetColumnWidths = {};
+  // Get all worksheets
+  const sheetNames = wb.SheetNames;
+  let sheetsData = {};
+  let sheetColumnWidths = {};
 
-      sheetNames.forEach((name) => {
-        const ws = wb.Sheets[name];
+  sheetNames.forEach(name => {
+    const ws = wb.Sheets[name];
 
-        const colsWidths = ws["!cols"]?.map((col) => col?.wpx || 100) || [];
-        sheetColumnWidths[name] = colsWidths;
+    const colsWidths = ws['!cols']?.map(col => col?.wpx || 100) || [];
+    sheetColumnWidths[name] = colsWidths;
 
-        let data = utils.sheet_to_json(ws, { header: 1 });
-        data = data.filter((row) => row.length);
+    let data = utils.sheet_to_json(ws, { header: 1 });
+    data = data.filter(row => row.length);
 
-        sheetsData[name] = data;
-      });
-      return {sheetsData, sheetColumnWidths};
-}
+    sheetsData[name] = data;
+  });
+  return { sheetsData, sheetColumnWidths };
+};
 
 function beginGroupRowRenderer(instance, td, row, col, prop, value, cellProperties) {
-  textRenderer.apply(this, arguments);  // Use the default text renderer first
+  textRenderer.apply(this, arguments); // Use the default text renderer first
 
   const rowData = instance.getDataAtRow(row); // Get data for the row
 
@@ -55,7 +54,7 @@ function beginGroupRowRenderer(instance, td, row, col, prop, value, cellProperti
   }
 }
 
-registerRenderer('beginGroupRowRenderer', beginGroupRowRenderer);  // Register the renderer
+registerRenderer('beginGroupRowRenderer', beginGroupRowRenderer); // Register the renderer
 
 function App() {
   const [hotData, setHotData] = useState({});
@@ -63,31 +62,33 @@ function App() {
   const [selectedFile, setSelectedFile] = useState('');
   const [previewUrl, setPreviewUrl] = useState('');
 
-  const handleFileUpload = (e) => {
+  const handleFileUpload = e => {
     const file = e.target.files[0];
     const reader = new FileReader();
-    reader.onload = (evt) => {
+    reader.onload = evt => {
       const bstr = evt.target.result;
-      const {sheetsData, sheetColumnWidths} = getSheetsData(bstr);
+      const { sheetsData, sheetColumnWidths } = getSheetsData(bstr);
       setHotData(sheetsData);
       setColWidths(sheetColumnWidths);
     };
     if (file) reader.readAsBinaryString(file);
     setSelectedFile('');
-  }
+  };
 
-  const handleSelectChange = async (event) => {
+  const handleSelectChange = async event => {
     const fileName = event.target.value;
     setSelectedFile(fileName);
     if (!fileName) return;
 
     const response = await fetch(`/xlsform_examples/${fileName}`);
     const blob = await response.blob();
-    var file = new File([blob], fileName, {type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"});
+    var file = new File([blob], fileName, {
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
     const reader = new FileReader();
-    reader.onload = async (evt) => {
+    reader.onload = async evt => {
       const bstr = evt.target.result;
-      const {sheetsData, sheetColumnWidths} = getSheetsData(bstr);
+      const { sheetsData, sheetColumnWidths } = getSheetsData(bstr);
       setHotData(sheetsData);
       setColWidths(sheetColumnWidths);
     };
@@ -96,8 +97,8 @@ function App() {
 
   const constructSpreadsheet = () => {
     const wb = utils.book_new();
-    
-    Object.keys(hotData).forEach((sheetName) => {
+
+    Object.keys(hotData).forEach(sheetName => {
       const ws = utils.aoa_to_sheet(hotData[sheetName]);
       utils.book_append_sheet(wb, ws, sheetName);
     });
@@ -105,34 +106,36 @@ function App() {
     const wbout = write(wb, { bookType: 'xlsx', bookSST: true, type: 'binary' });
     const buf = new ArrayBuffer(wbout.length);
     const view = new Uint8Array(buf);
-    
-    for(let i=0; i<wbout.length; i++) view[i] = wbout.charCodeAt(i) & 0xFF;
-    const fileBlob = new Blob([buf], {type:"application/octet-stream"});
-    return fileBlob;
 
-  }
+    for (let i = 0; i < wbout.length; i++) view[i] = wbout.charCodeAt(i) & 0xff;
+    const fileBlob = new Blob([buf], { type: 'application/octet-stream' });
+    return fileBlob;
+  };
 
   const handleFileDownload = () => {
     const fileBlob = constructSpreadsheet();
-    saveAs(fileBlob, "spreadsheet.xlsx");
-  }
+    saveAs(fileBlob, 'spreadsheet.xlsx');
+  };
 
   const handleFilePreview = async () => {
     const fileBlob = constructSpreadsheet();
 
     let formData = new FormData();
-    formData.append("file", fileBlob, "spreadsheet.xlsx");
+    formData.append('file', fileBlob, 'spreadsheet.xlsx');
     try {
-      const url = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api/xform/' : 'https://xlsform-online.fly.dev/api/xform/';
+      const url =
+        process.env.NODE_ENV === 'development'
+          ? 'http://localhost:8000/api/xform/'
+          : 'https://xlsform-online.fly.dev/api/xform/';
       const response = await fetch(url, {
         method: 'POST',
-        body: formData
+        body: formData,
       });
 
       // if it's a 400 error, show the message in response.json()["error"]
       if (response.status === 400) {
         const data = await response.json();
-        alert(data["error"]);
+        alert(data['error']);
         return;
       }
 
@@ -144,21 +147,20 @@ function App() {
       const xml_url = data.xml_url;
       const previewUrl = `https://staging.enketo.getodk.org/preview?form=${xml_url}`;
       setPreviewUrl(previewUrl);
-
     } catch (error) {
-      alert('There was a problem with the request:', error)
+      alert('There was a problem with the request:', error);
     }
-  }
- 
+  };
+
   return (
     <Allotment defaultSizes={[100, 200]} className="scrollable-allotment">
       <Allotment.Pane minSize={200} preferredSize="80%">
         <div className="App">
-          <Grid container spacing={1} direction="row" alignItems={"center"} marginBottom={2}>
-            <Grid item md={2} xs={4} textAlign={"center"}>
+          <Grid container spacing={1} direction="row" alignItems={'center'} marginBottom={2}>
+            <Grid item md={2} xs={4} textAlign={'center'}>
               <input
                 accept=".xls,.xlsx"
-                style={{ display: "none" }}
+                style={{ display: 'none' }}
                 id="raised-button-file"
                 type="file"
                 onChange={handleFileUpload}
@@ -169,49 +171,31 @@ function App() {
                 </Button>
               </label>
             </Grid>
-            <Grid item md={1} xs={2} textAlign={"center"}>
+            <Grid item md={1} xs={2} textAlign={'center'}>
               <Box> OR </Box>
             </Grid>
-            <Grid item md={5} xs={7} textAlign={"center"}>
+            <Grid item md={5} xs={7} textAlign={'center'}>
               <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-                <InputLabel id="example-file-select">
-                  Select an example file
-                </InputLabel>
-                <Select
-                  labelId="example-file-select"
-                  value={selectedFile}
-                  onChange={handleSelectChange}
-                >
+                <InputLabel id="example-file-select">Select an example file</InputLabel>
+                <Select labelId="example-file-select" value={selectedFile} onChange={handleSelectChange}>
                   <MenuItem value="anc_visit.xlsx">anc_visit.xlsx</MenuItem>
-                  <MenuItem value="baseline_household_survey.xlsx">
-                    baseline_household_survey.xlsx
-                  </MenuItem>
-                  <MenuItem value="fatal_injury_surveillance_form.xlsx">
-                    fatal_injury_surveillance_form.xlsx
-                  </MenuItem>
-                  <MenuItem value="household_water_survey.xlsx">
-                    household_water_survey.xlsx
-                  </MenuItem>
-                  <MenuItem value="monthly_project_report.xlsx">
-                    monthly_project_report.xlsx
-                  </MenuItem>
-                  <MenuItem value="shelter_material_survey.xlsx">
-                    shelter_material_survey.xlsx
-                  </MenuItem>
-                  <MenuItem value="spraying_survey.xlsx">
-                    spraying_survey.xlsx
-                  </MenuItem>
+                  <MenuItem value="baseline_household_survey.xlsx">baseline_household_survey.xlsx</MenuItem>
+                  <MenuItem value="fatal_injury_surveillance_form.xlsx">fatal_injury_surveillance_form.xlsx</MenuItem>
+                  <MenuItem value="household_water_survey.xlsx">household_water_survey.xlsx</MenuItem>
+                  <MenuItem value="monthly_project_report.xlsx">monthly_project_report.xlsx</MenuItem>
+                  <MenuItem value="shelter_material_survey.xlsx">shelter_material_survey.xlsx</MenuItem>
+                  <MenuItem value="spraying_survey.xlsx">spraying_survey.xlsx</MenuItem>
                 </Select>
               </FormControl>
             </Grid>
             {Object.keys(hotData).length > 0 && (
               <>
-                <Grid item md={2} xs={6} textAlign={"center"}>
+                <Grid item md={2} xs={6} textAlign={'center'}>
                   <Button variant="outlined" onClick={handleFileDownload}>
                     Download
                   </Button>
                 </Grid>
-                <Grid item md={2} xs={6} textAlign={"center"}>
+                <Grid item md={2} xs={6} textAlign={'center'}>
                   <Button variant="contained" onClick={handleFilePreview}>
                     Preview
                   </Button>
@@ -222,16 +206,15 @@ function App() {
 
           <Tabs>
             <TabList>
-              {Object.keys(hotData).map((sheetName) => (
+              {Object.keys(hotData).map(sheetName => (
                 <Tab key={sheetName}>{sheetName}</Tab>
               ))}
             </TabList>
-            {Object.keys(hotData).map((sheetName) => (
+            {Object.keys(hotData).map(sheetName => (
               <TabPanel key={`${selectedFile}_${sheetName}`}>
                 <HotTable
                   data={hotData[sheetName]}
                   rowHeaders={true}
-                  colHeaders={true}
                   height="100vh"
                   colWidths={colWidths[sheetName]}
                   licenseKey="non-commercial-and-evaluation" // for non-commercial use only
@@ -240,7 +223,7 @@ function App() {
                   manualRowMove={true}
                   manualColumnResize={true}
                   fixedRowsTop={1}
-                  cells={function(row, col) {
+                  cells={function (row, col) {
                     const cellProperties = {};
 
                     if (this.instance.getDataAtCell(row, 0) === 'begin group') {
@@ -257,15 +240,7 @@ function App() {
       </Allotment.Pane>
       {previewUrl && (
         <Allotment.Pane minSize={200}>
-          <div>
-            {previewUrl && (
-              <iframe
-                src={previewUrl}
-                title="Preview"
-                className="full-size-iframe"
-              />
-            )}
-          </div>
+          <div>{previewUrl && <iframe src={previewUrl} title="Preview" className="full-size-iframe" />}</div>
         </Allotment.Pane>
       )}
     </Allotment>
